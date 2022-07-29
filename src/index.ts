f6b0f74f992ea926c6a9e5972bea131e824407df
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-07-29 15:43:57 +0900

    export JsonType

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { HacoCmsClient } from './api-client'
 export { ApiContent } from './api-content'
 export { SortQuery } from './query'
-export { toDate, toNullableDate } from './json-utils'
+export { JsonType, toDate, toNullableDate } from './json-utils'
