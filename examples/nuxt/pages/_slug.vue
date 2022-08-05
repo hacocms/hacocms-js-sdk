bd6a72541e7d2da1f847b5cdd573f06c93dc397c
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-08-05 16:42:55 +0900

    docs: add Nuxt SSG example

---
+++ examples/nuxt/pages/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <h1>{{ entry.title }}</h1>
+      <p>{{ entry.description }}</p>
+    </header>
+
+    <div v-html="entry.body"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EntryPage',
+  asyncData({ payload }) {
+    return payload
+  },
+}
+</script>
---
+++ examples/nuxt/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Nuxt.js Example</h1>
+
+    <div v-for="entry in entries" :key="entry.id">
+      <NuxtLink :to="`/${entry.id}`">
+        <div class="title">{{ entry.title }}</div>
+        <p>{{ entry.description }}</p>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  asyncData({ payload }) {
+    return payload
+  },
+}
+</script>
+
+<style scoped>
+.title {
+  font-size: larger;
+}
+</style>
